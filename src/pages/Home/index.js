fb4727afa732fa433706d6613fd2097b5517027a
Luan Lemos <luan.lemos@agenciaeplus.com.br>
2022-10-08 13:32:09 -0300

    Adicionar mensagem de carregamento

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,6 +6,7 @@ import './style.css'
 
 function Home() {
   const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     async function loadMovies() {
@@ -18,10 +19,19 @@ function Home() {
       })
 
       setMovies(response.data.results.slice(0, 10))
+      setLoading(false)
     }
 
     loadMovies()
   }, [])
+
+  if(loading) {
+    return (
+      <div className="loading">
+        <h2>Carregando filmes...</h2>
+      </div>
+    )
+  }
 
   return (
     <div className="container">
