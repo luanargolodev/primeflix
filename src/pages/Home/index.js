abe0d6861d6483d4cba38285bcecb8e7ea22f52e
Luan Lemos <luan.lemos@agenciaeplus.com.br>
2022-10-08 13:04:08 -0300

    Consumindo API e listando filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,25 @@
+import { useEffect, useState } from 'react'
+import api from '../../services/api'
+
 function Home() {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    async function loadMovies() {
+      const response = await api.get('movie/now_playing', {
+        params: {
+          api_key: '87533c310d3e816e461feca9a728b65f',
+          language: 'pt-BR',
+          page: 1
+        }
+      })
+
+      console.log(response.data)
+    }
+
+    loadMovies()
+  }, [])
+
   return (
     <div>
       <h1>BEM VINDO A HOME</h1>
